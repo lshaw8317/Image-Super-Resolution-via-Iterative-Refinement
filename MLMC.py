fa5924543cd5a8f1e3fd89763223046aff7703c2
Luke Shaw <lshaw8317@gmail.com>
2023-08-23 09:47:39 +0100

    Editing MLMC

--- MLMC.py
+++ MLMC.py
@@ -22,25 +22,26 @@ def imagenorm(img):
 
 def Giles_plot(diff,acc):
         #Set mlmc params
-        M=diff.netG.M
-        N0=diff.netG.N0
-        Lmax=diff.netG.Lmax
+        M=diff.netG.module.M
+        N0=diff.netG.module.N0
+        Lmax=diff.netG.module.Lmax
+        eval_dir = diff.netG.module.eval_dir
         Nsamples=10**3
         condition_x=diff.data['SR']
-        min_l=diff.netG.min_l
+        min_l=diff.netG.module.min_l
 
         #Variance and mean samples
-        sums,sqsums=diff.netG.mlmclooper(condition_x,l=1,Nl=1,min_l=0) #dummy run to get sum shapes 
+        sums,sqsums=diff.netG.module.mlmclooper(condition_x,l=1,Nl=1,min_l=0) #dummy run to get sum shapes 
         sums=torch.zeros((Lmax+1-min_l,*sums.shape))
         sqsums=torch.zeros((Lmax+1-min_l,*sqsums.shape))
         # Directory to save means and norms                                                                                               
-        this_sample_dir = os.path.join(diff.netG.eval_dir, f"VarMean_M_{M}_Nsamples_{Nsamples}")
+        this_sample_dir = os.path.join(eval_dir, f"VarMean_M_{M}_Nsamples_{Nsamples}")
         if not os.path.exists(this_sample_dir):
             os.mkdir(this_sample_dir)
             print(f'Proceeding to calculate variance and means with {Nsamples} estimator samples')
             for i,l in enumerate(range(min_l,Lmax+1)):
                 print(f'l={l}')
-                sums[i],sqsums[i] = diff.netG.mlmclooper(condition_x,Nsamples,l)
+                sums[i],sqsums[i] = diff.netG.module.mlmclooper(condition_x,Nsamples,l)
 
             sumdims=tuple(range(1,len(sqsums[:,0].shape))) #sqsums is output of payoff element-wise squared, so reduce     
             s=sqsums[:,0].shape
@@ -79,7 +80,7 @@ def Giles_plot(diff,acc):
         for i in range(len(acc)):
             e=acc[i]
             print(f'Performing mlmc for accuracy={e}')
-            sums,sqsums,N=diff.netG.mlmc(e,condition_x,alpha_0=alpha,beta_0=beta) #sums=[dX,Xf,Xc], sqsums=[||dX||^2,||Xf||^2,||Xc||^2]
+            sums,sqsums,N=diff.netG.module.mlmc(e,condition_x,alpha_0=alpha,beta_0=beta) #sums=[dX,Xf,Xc], sqsums=[||dX||^2,||Xf||^2,||Xc||^2]
             sumdims=tuple(range(1,len(sqsums[:,0].shape))) #sqsums is output of payoff element-wise squared, so reduce
             s=sqsums[:,0].shape
 
@@ -87,7 +88,7 @@ def Giles_plot(diff,acc):
             dividerN=N.clone() #add axes to N to broadcast correctly on division
             for i in range(len(sums.shape[1:])):
                 dividerN.unsqueeze_(-1)
-            this_sample_dir = os.path.join(diff.netG.eval_dir, f"M_{M}_accuracy_{e}")
+            this_sample_dir = os.path.join(eval_dir, f"M_{M}_accuracy_{e}")
             
             if not os.path.exists(this_sample_dir):
                 os.mkdir(this_sample_dir)        
@@ -160,16 +161,16 @@ if __name__ == "__main__":
     current_epoch = diffusion.begin_epoch
     logger.info('Initial Model Finished')
     #Modify noise schedule to correspond to MLMC max L in diffusion
-    opt['model']['beta_schedule'][opt['phase']]['n_timestep']=diffusion.netG.M**diffusion.netG.Lmax
+    opt['model']['beta_schedule'][opt['phase']]['n_timestep']=diffusion.netG.module.M**diffusion.netG.module.Lmax
     
     diffusion.set_new_noise_schedule(
         opt['model']['beta_schedule'][opt['phase']], schedule_phase=opt['phase'])
-    print(f'Beta length = {len(diffusion.netG.betas)}')
+    print(f'Beta length = {len(diffusion.netG.module.betas)}')
     logger.info('Begin Model Evaluation.')
     avg_psnr = 0.0
     avg_ssim = 0.0
     idx = 0
-    result_path = diffusion.netG.eval_dir
+    result_path = diffusion.netG.module.eval_dir
     os.makedirs(result_path, exist_ok=True)
     for _,  val_data in enumerate(val_loader):
         #val_data automatically has batch size 1 for phase=val
